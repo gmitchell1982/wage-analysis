8cc141d0facd5b434f5d43070723a7630149a388
Gerald L. Mitchell <gmitchell1982@gmail.com>
2021-07-09 14:05:08 -0400

    left join data from Person.BusinessEntityAddress table

--- data-gathering.sql
+++ data-gathering.sql
@@ -10,7 +10,10 @@ CurrentEmployees.BusinessEntityID, CurrentEmployees.NationalIDNumber, CurrentEmp
 PayHistory.Rate, PayHistory.PayFrequency, PayHistory.RateChangeDate,
 
 --columns wanted from the person.person table
-Person.PersonType, Person.Demographics
+Person.PersonType, Person.Demographics,
+
+--columns wanted from the person.businessentityaddress table
+AddressKey.AddressID, AddressKey.AddressTypeID
 
 --use the hr.employee table as the left table
 from AdventureWorks2019.HumanResources.Employee as CurrentEmployees
@@ -26,6 +29,12 @@ left join
 --add columns that include demographic information for current employees
 AdventureWorks2019.Person.Person as Person
 on CurrentEmployees.BusinessEntityID = Person.BusinessEntityID
+
+left join
+
+--add columns that include address reference information for current employees
+AdventureWorks2019.Person.BusinessEntityAddress as AddressKey
+on CurrentEmployees.BusinessEntityID = AddressKey.BusinessEntityID
 
 --only want info for current employees (CurrentFlag = 1)
 where CurrentFlag = 1
