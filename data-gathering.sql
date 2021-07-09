6c61fe25c1a78ac097300497e6311221ac4960ee
Gerald L. Mitchell <gmitchell1982@gmail.com>
2021-07-09 13:49:15 -0400

    select desired end result columns from hr.employee table for current employees only

--- data-gathering.sql
+++ data-gathering.sql
@@ -1,2 +1,11 @@
 -- this document contains the SQL code to collect the needed data from the database into a single exportable table
 
+
+select
+
+CurrentEmployees.BusinessEntityID, CurrentEmployees.NationalIDNumber, CurrentEmployees.OrganizationNode, CurrentEmployees.OrganizationLevel, CurrentEmployees.JobTitle, CurrentEmployees.BirthDate, CurrentEmployees.MaritalStatus, CurrentEmployees.Gender, CurrentEmployees.HireDate, CurrentEmployees.SalariedFlag, CurrentEmployees.VacationHours, CurrentEmployees.SickLeaveHours
+
+from AdventureWorks2019.HumanResources.Employee as CurrentEmployees
+
+--only want info for current employees (CurrentFlag = 1)
+where CurrentFlag = 1
