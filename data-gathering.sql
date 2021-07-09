8fa1c3cb648ee16ed42160a9d1580ecd6bef8e09
Gerald L. Mitchell <gmitchell1982@gmail.com>
2021-07-09 14:01:29 -0400

    left join data from Person.Person table

--- data-gathering.sql
+++ data-gathering.sql
@@ -7,7 +7,10 @@ select
 CurrentEmployees.BusinessEntityID, CurrentEmployees.NationalIDNumber, CurrentEmployees.OrganizationNode, CurrentEmployees.OrganizationLevel, CurrentEmployees.JobTitle, CurrentEmployees.BirthDate, CurrentEmployees.MaritalStatus, CurrentEmployees.Gender, CurrentEmployees.HireDate, CurrentEmployees.SalariedFlag, CurrentEmployees.VacationHours, CurrentEmployees.SickLeaveHours,
 
 --columns wanted from the hr.employeepayhistory table
-PayHistory.Rate, PayHistory.PayFrequency, PayHistory.RateChangeDate
+PayHistory.Rate, PayHistory.PayFrequency, PayHistory.RateChangeDate,
+
+--columns wanted from the person.person table
+Person.PersonType, Person.Demographics
 
 --use the hr.employee table as the left table
 from AdventureWorks2019.HumanResources.Employee as CurrentEmployees
@@ -17,6 +20,12 @@ left join
 --add columns that include pay history information for current employees
 AdventureWorks2019.HumanResources.EmployeePayHistory as PayHistory
 on CurrentEmployees.BusinessEntityID = PayHistory.BusinessEntityID
+
+left join
+
+--add columns that include demographic information for current employees
+AdventureWorks2019.Person.Person as Person
+on CurrentEmployees.BusinessEntityID = Person.BusinessEntityID
 
 --only want info for current employees (CurrentFlag = 1)
 where CurrentFlag = 1
